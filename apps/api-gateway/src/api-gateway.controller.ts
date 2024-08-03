4bcca6d077966726bdb7fdac98e2babf9c32e6a1
Aftertnw <64160049@go.buu.ac.th>
2024-08-03 15:59:13 +0700

    setup api gateway controller

--- apps/api-gateway/src/api-gateway.controller.ts
+++ apps/api-gateway/src/api-gateway.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiGatewayService } from './api-gateway.service';
+// import { ApiGatewayService } from './api-gateway.service';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Controller()
 export class ApiGatewayController {
-  constructor(private readonly apiGatewayService: ApiGatewayService) {}
-
-  @Get()
-  getHello(): string {
-    return this.apiGatewayService.getHello();
+  constructor(private httpService: HttpService) {}
+  @Get('menu')
+  async getMenu() {
+    const response = await firstValueFrom(
+      this.httpService.get('http://localhost:3001/menu'),
+    );
+    return response.data;
   }
 }
--- apps/menu/src/menu.service.ts
+++ apps/menu/src/menu.service.ts
@@ -7,18 +7,14 @@ export class MenuService {
     { id: 2, name: 'Burger', price: 8.99 },
     { id: 3, name: 'Salad', price: 7.99 },
   ];
-  getMnenu() {
-    return this.menu;
-  }
-
   getMenuItem(id: number) {
     const item = this.menu.find((item) => item.id === id);
     if (!item) {
-      throw new NotFoundException(`Menu item not found`);
+      throw new NotFoundException('Menu item not found');
     }
     return item;
   }
-  getHello(): string {
-    return 'Hello World!';
+  getMenu() {
+    return this.menu;
   }
 }
