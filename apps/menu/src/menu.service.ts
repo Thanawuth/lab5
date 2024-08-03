d2e2fbe8fc5a2e95563b2d4cae085b8ec4a39197
Aftertnw <64160049@go.buu.ac.th>
2024-08-03 15:48:18 +0700

    set and get item

---
+++ apps/menu/src/menu.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class MenuService {
+  private menu = [
+    { id: 1, name: 'Pizza', price: 10.99 },
+    { id: 2, name: 'Burger', price: 8.99 },
+    { id: 3, name: 'Salad', price: 7.99 },
+  ];
+  getMnenu() {
+    return this.menu;
+  }
+
+  getMenuItem(id: number) {
+    const item = this.menu.find((item) => item.id === id);
+    if (!item) {
+      throw new NotFoundException(`Menu item not found`);
+    }
+    return item;
+  }
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
